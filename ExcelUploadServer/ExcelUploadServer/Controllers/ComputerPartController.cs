b03d4019fdb3f4aedd3e3eb04e50343b87d0763e
Sandor Kis <kissandor@hotmail.com>
2023-10-18 21:16:45 +0100

    Feat: Add CreateEdit Endpoint for ComputerPartController

--- ExcelUploadServer/ExcelUploadServer/Controllers/ComputerPartController.cs
+++ ExcelUploadServer/ExcelUploadServer/Controllers/ComputerPartController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using ExcelUploadServer.Data;
+using ExcelUploadServer.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ExcelUploadServer.Controllers
@@ -7,5 +9,39 @@ namespace ExcelUploadServer.Controllers
     [ApiController]
     public class ComputerPartController : ControllerBase
     {
+        private readonly ExcelUploadContext _context;
+
+        public ComputerPartController(ExcelUploadContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost]
+        public JsonResult CreateEdit(IEnumerable<ComputerPart> computerPartList)
+        {
+            if (computerPartList == null || !computerPartList.Any())
+            {
+                return new JsonResult(BadRequest("Nincs elmentendő érték."));
+            }
+
+            foreach (var computerPart in computerPartList)
+            {
+                if (computerPart.Id == 0)
+                {
+                    _context.ComputerParts.Add(computerPart);
+                }
+                else
+                {
+                    var computerPartInDb = _context.ComputerParts.Find(computerPart.Id);
+                    if (computerPartInDb == null)
+                        return new JsonResult(NotFound());
+                    computerPartInDb = computerPart;
+                }
+            }
+
+            _context.SaveChanges();
+
+            return new JsonResult(Ok(computerPartList));
+        }
     }
 }
